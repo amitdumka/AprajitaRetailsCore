dc084c38ea99c0e60f85efead3d04d620bbe9830
Amit Kumar <amitnarayansah@gmail.com>
2020-10-30 21:00:17 +0530

    Add project files.

---
+++ AprajitaRetails/Data/AprajitaRetailsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AprajitaRetails.Data
+{
+    public class AprajitaRetailsContext : IdentityDbContext
+    {
+        public AprajitaRetailsContext(DbContextOptions<AprajitaRetailsContext> options)
+            : base(options)
+        {
+        }
+    }
+}
