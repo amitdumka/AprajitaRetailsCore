d5e321457a1b611c9ef0acede36c0350fcb51e32
Amit Kumar <amitnarayansah@gmail.com>
2020-11-01 20:49:57 +0530

    Some Changes

--- AprajitaRetails.Shared/Data/Types/ArvindAccount.cs
+++ AprajitaRetails.Shared/Data/Types/ArvindAccount.cs
@@ -1,3 +1,7 @@
-﻿
+﻿/// <summary>
+/// Status of Data Entry Maded by User.
+/// </summary>
+public enum Status { Added, Approved, Rejected, Updated, Deleted }
+
 public enum ArvindAccount { ArvindLimited, ALBL, AFL, Others }
 
