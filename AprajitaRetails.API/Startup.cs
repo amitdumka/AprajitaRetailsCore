0815f8fe02cf0708c7fb1d88062dcc5fc21b2bc6
Aprajita Retails <aprajitaretails@outlook.com>
2020-11-18 12:47:38 +0530

    swagger added to api

--- AprajitaRetails.API/Startup.cs
+++ AprajitaRetails.API/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace AprajitaRetails.API
 {
@@ -28,26 +30,47 @@ namespace AprajitaRetails.API
             services.AddDbContext<AprajitaRetailsContext>(options =>
                options.UseSqlServer(
                    Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                 .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<AprajitaRetailsContext>();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Aprajita Retails ERP API",
+                    Description = "Apraita Retails ERP Api for web and mobile app.",
+                    TermsOfService = new Uri("https://www.aprajitaretails.in/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Amit Kumar",
+                        Email = "amitnarayansah@gmail.com",
+                        Url = new Uri("https://twitter.com/amitdumka"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://aprajitaretails.in/license"),
+                    }
+                });
+            });
+            //services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //     .AddRoles<IdentityRole>()
+            //    .AddEntityFrameworkStores<AprajitaRetailsContext>();
             services.AddDistributedMemoryCache();
-            services.AddAuthorization(options =>
-            {
-                options.FallbackPolicy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-            });
-            services.AddControllers(config =>
-            {
-                // using Microsoft.AspNetCore.Mvc.Authorization;
-                // using Microsoft.AspNetCore.Authorization;
-                var policy = new AuthorizationPolicyBuilder()
-                                 .RequireAuthenticatedUser()
-                                 .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-            });
+            //services.AddAuthorization(options =>
+            //{
+            //    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+            //        .RequireAuthenticatedUser()
+            //        .Build();
+            //});
+            ////services.AddControllers(config =>
+            //{
+            //    // using Microsoft.AspNetCore.Mvc.Authorization;
+            //    // using Microsoft.AspNetCore.Authorization;
+            //    var policy = new AuthorizationPolicyBuilder()
+            //                     .RequireAuthenticatedUser()
+            //                     .Build();
+            //    config.Filters.Add(new AuthorizeFilter(policy));
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,12 +84,28 @@ namespace AprajitaRetails.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
-            
-            app.UseAuthentication(); 
-            app.UseAuthorization();
+            //app.UseAuthentication(); 
+            //app.UseAuthorization();
 
-            app.UseSession();
+            // app.UseSession();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            //app.UseSwagger(c =>
+            //{
+            //    c.SerializeAsV2 = true;
+            //});
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aprajita Retails ERP V5.0");
+                c.RoutePrefix = string.Empty;
+                //c.InjectStylesheet("/swagger-ui/custom.css");
+            });
 
             app.UseEndpoints(endpoints =>
             {
---
+++ AprajitaRetails.Tally/Ops/TallyManager.cs
@@ -0,0 +1,39 @@
+﻿using AprajitaRetails.DL.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AprajitaRetails.Shared.Models.Payrolls;
+
+namespace AprajitaRetails.Tally.Ops
+{
+    public interface ITallyManager<T>
+    {
+        public bool Add(AprajitaRetailsContext db , T objectValue);
+        public bool Delete(AprajitaRetailsContext db, T objectValue);
+        public bool Update(AprajitaRetailsContext db, T objectValue);
+        public bool AddOrUpdate(AprajitaRetailsContext db, T objectValue);
+    }
+
+    public class PayrollManager : ITallyManager<Attendance>
+    {
+        public bool Add(AprajitaRetailsContext db, Attendance objectValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddOrUpdate(AprajitaRetailsContext db, Attendance objectValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(AprajitaRetailsContext db, Attendance objectValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(AprajitaRetailsContext db, Attendance objectValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
